ea0188a7c23dd37e809e641b3522f514de23c517
Chris Hocking <latenitefilms@users.noreply.github.com>
2023-05-29 15:16:11 +1000

    Update generate_rss.js

--- .github/scripts/generate_rss.js
+++ .github/scripts/generate_rss.js
@@ -3,46 +3,46 @@ const RSS = require('rss');
 const MarkdownIt = require('markdown-it');
 const md = new MarkdownIt();
 
-const generateRSS = () => {
-  // Initialize RSS feed
-  let feed = new RSS({
-    title: 'FCP Cafe',
-    description: 'The latest news from FCP Cafe',
-    feed_url: 'https://fcp.cafe/rss.xml',
-    site_url: 'https://fcp.cafe',
-  });
+function convertDateToRFC822(dateString) {
+    let date = new Date(dateString);
+    return date.toUTCString();
+}
 
-  // Read and parse the Markdown file
-  const fileContent = fs.readFileSync('docs/README.md', 'utf8');
-  const tokens = md.parse(fileContent, {});
+function generateUrl(title) {
+    return 'https://fcp.cafe/#' + title.toLowerCase().replace(/ /g, '-');
+}
 
-  // Loop through the tokens and add to the RSS feed
-  for (let i = 0; i < tokens.length; i++) {
-    if (tokens[i].type === 'heading_open' && tokens[i].tag === 'h3') {
-      let title = tokens[i + 1].content;
-      let description = tokens[i + 3].content;
-
-      // Extract and parse date from title
-      const datePattern = /\w+ \d{1,2}\w* \w+ \d{4}/g;
-      const dateMatch = title.match(datePattern);
-      let date;
-      if (dateMatch) {
-        date = new Date(dateMatch[0]);
-      } else {
-        date = new Date();
-      }
-
-      feed.item({
-        title: title,
-        description: description,
-        date: date,
-      });
+fs.readFile('docs/README.md', 'utf8', function(err, data) {
+    if (err) {
+        console.error(err);
+        return;
     }
-  }
 
-  // Generate XML
-  let xml = feed.xml({ indent: true });
-  fs.writeFileSync('docs/rss.xml', xml);
-};
+    const feed = new RSS({
+        title: 'FCP Cafe',
+        description: 'The latest news from FCP Cafe',
+        feed_url: 'https://fcp.cafe/rss.xml',
+        site_url: 'https://fcp.cafe',
+        generator: 'RSS for Node'
+    });
 
-generateRSS();
+    const entries = data.split('\n### ');
+
+    for (const entry of entries) {
+        const lines = entry.split('\n');
+        const title = lines[0].trim();
+        const date = convertDateToRFC822(title);
+        const content = md.render(lines.slice(1).join('\n').trim());
+        const url = generateUrl(title);
+
+        feed.item({
+            title: title,
+            guid: title,
+            description: content,
+            url: url,
+            date: date
+        });
+    }
+
+    fs.writeFileSync('docs/rss.xml', feed.xml({indent: true}));
+});
