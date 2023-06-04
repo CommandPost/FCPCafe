f4881e0f12fbe032d9f058d81a13d090adce9eab
Chris Hocking <latenitefilms@users.noreply.github.com>
2023-06-05 07:17:43 +1000

    Simplified RSS code

--- .github/scripts/generate_rss.js
+++ .github/scripts/generate_rss.js
@@ -2,12 +2,11 @@ const fs = require('fs');
 const RSS = require('rss');
 const MarkdownIt = require('markdown-it');
 const FeedParser = require('feedparser');
-const deepEqual = require('deep-equal'); // Requires the deep-equal package
+const deepEqual = require('deep-equal');
 
-const md = new MarkdownIt({html: true}); // Allow inline HTML
+const md = new MarkdownIt({html: true});
 
 function convertDateToRFC822(dateString) {
-    // Remove ordinal indicators
     dateString = dateString.replace(/\b(\d+)(st|nd|rd|th)\b/g, "$1");
     let date = new Date(dateString);
     return date.toUTCString();
@@ -19,8 +18,8 @@ function generateUrl(title) {
 
 let pubDate = null;
 let lastBuildDate = null;
-let isContentChanged = false;  // New variable to track changes
-let oldFeedItems = []; // Store old feed items
+let isContentChanged = false;
+let oldFeedItems = [];
 
 if (fs.existsSync('docs/rss.xml')) {
     const feedparser = new FeedParser();
@@ -62,24 +61,20 @@ fs.readFile('docs/README.md', 'utf8', function(err, data) {
     for (const entry of entries) {
         const lines = entry.trim().split('\n');
 
-        // if the line starts with '###', it's a new date
         if (lines[0].startsWith('### ')) {
             currentTitle = lines[0].substring(4);
             currentDate = convertDateToRFC822(currentTitle);
-            lines.shift();  // remove the date line
+            lines.shift();
         }
 
-        // skip empty entries or entries starting with an include line
         if (lines.length === 0 || lines[0].startsWith('{{ include')) {
             continue;
         }
 
         let content = lines.join('\n').trim();
 
-        // Render markdown to HTML
         content = md.render(content);
 
-        // Handle non-standard markdown after HTML rendering
         content = content.replace(/{{ include ".*" }}/g, '')
             .replace(/\!\[([^\]]*)\]\(([^)]*)\)/g, '<img src="https://fcp.cafe/$2" alt="$1">')
             .replace(/\[\!button text="([^"]*)" target="([^"]*)" variant="([^"]*)"\]\(([^)]*)\)/g, '<a href="$4">$1</a>')
@@ -95,29 +90,22 @@ fs.readFile('docs/README.md', 'utf8', function(err, data) {
             date: currentDate
         };
 
-        // Check if the item already exists in the feed
         const existingEntry = oldFeedItems.find(item => item.guid === newEntry.guid);
 
-        // If the entry is new or the content has changed, mark the content as changed
         if (!existingEntry || !deepEqual(existingEntry, newEntry)) {
             isContentChanged = true;
+            feed.item(newEntry);
         }
-
-        feed.item(newEntry);
     }
 
-    // Update lastBuildDate only if content has changed
     if (isContentChanged) {
         feed.date = new Date();
     } else if (lastBuildDate !== null) {
         feed.date = lastBuildDate;
     }
 
-    const newXMLContent = feed.xml({indent: true});
-    const oldXMLContent = fs.existsSync('docs/rss.xml') ? fs.readFileSync('docs/rss.xml', 'utf8') : '';
-
-    // Only write to the file if the content has changed
-    if (newXMLContent !== oldXMLContent) {
+    if (isContentChanged) {
+        const newXMLContent = feed.xml({indent: true});
         fs.writeFileSync('docs/rss.xml', newXMLContent);
     }
 });
