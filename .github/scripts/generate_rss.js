da401c8bdbbcabb0cfb3e89678df6474b39326cd
Chris Hocking <latenitefilms@users.noreply.github.com>
2023-06-05 00:09:12 +1000

    Tweaked RSS script

--- .github/scripts/generate_rss.js
+++ .github/scripts/generate_rss.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const RSS = require('rss');
 const MarkdownIt = require('markdown-it');
 const FeedParser = require('feedparser');
+const deepEqual = require('deep-equal'); // Requires the deep-equal package
 
 const md = new MarkdownIt({html: true}); // Allow inline HTML
 
@@ -16,41 +17,45 @@ function generateUrl(title) {
     return 'https://fcp.cafe/#' + title.toLowerCase().replace(/ /g, '-');
 }
 
+let pubDate = null;
+let lastBuildDate = null;
+let isContentChanged = false;  // New variable to track changes
+let oldFeedItems = []; // Store old feed items
+
+if (fs.existsSync('docs/rss.xml')) {
+    const feedparser = new FeedParser();
+
+    fs.createReadStream('docs/rss.xml').pipe(feedparser);
+
+    feedparser.on('meta', function(meta) {
+        pubDate = meta.pubdate || new Date();
+        lastBuildDate = meta.date || new Date();
+    });
+
+    feedparser.on('readable', function() {
+        let post;
+        while (post = this.read()) {
+            oldFeedItems.push(post);
+        }
+    });
+}
+
+const feed = new RSS({
+    title: 'FCP Cafe',
+    description: 'Latest News from FCP Cafe',
+    feed_url: 'https://fcp.cafe/rss.xml',
+    site_url: 'https://fcp.cafe',
+    generator: 'FCP Cafe',
+    pubDate: pubDate,
+});
+
 fs.readFile('docs/README.md', 'utf8', function(err, data) {
     if (err) {
         console.error(err);
         return;
     }
 
-    let pubDate = null;
-    let lastBuildDate = null;
-
-    if (fs.existsSync('docs/rss.xml')) {
-        const feedparser = new FeedParser();
-
-        fs.createReadStream('docs/rss.xml').pipe(feedparser);
-
-        feedparser.on('meta', function(meta) {
-            pubDate = meta.pubdate || new Date();
-            lastBuildDate = meta.date || new Date();
-        });
-    }
-
-    const feed = new RSS({
-        title: 'FCP Cafe',
-        description: 'Latest News from FCP Cafe',
-        feed_url: 'https://fcp.cafe/rss.xml',
-        site_url: 'https://fcp.cafe',
-        generator: 'FCP Cafe'
-    });
-
-    if (pubDate !== null) {
-        feed.pubDate = pubDate;
-        feed.date = lastBuildDate;
-    }
-
     const entries = data.split('\n---\n');
-
     let currentTitle = '';
     let currentDate = '';
 
@@ -82,13 +87,30 @@ fs.readFile('docs/README.md', 'utf8', function(err, data) {
 
         const url = generateUrl(currentTitle);
 
-        feed.item({
+        const newEntry = {
             title: currentTitle,
             guid: currentTitle,
             description: content,
             url: url,
             date: currentDate
-        });
+        };
+
+        // Check if the item already exists in the feed
+        const existingEntry = oldFeedItems.find(item => item.guid === newEntry.guid);
+
+        // If the entry is new or the content has changed, mark the content as changed
+        if (!existingEntry || !deepEqual(existingEntry, newEntry)) {
+            isContentChanged = true;
+        }
+
+        feed.item(newEntry);
+    }
+
+    // Update lastBuildDate only if content has changed
+    if (isContentChanged) {
+        feed.date = new Date();
+    } else if (lastBuildDate !== null) {
+        feed.date = lastBuildDate;
     }
 
     const newXMLContent = feed.xml({indent: true});
