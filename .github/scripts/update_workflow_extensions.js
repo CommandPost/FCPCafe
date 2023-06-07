52b33ad9b042237e8d23eec843d2f07dd8b2487b
Chris Hocking <latenitefilms@users.noreply.github.com>
2023-06-07 11:55:08 +1000

    Attempting to fix Workflow Extensions generator

--- .github/scripts/update_workflow_extensions.js
+++ .github/scripts/update_workflow_extensions.js
@@ -4,26 +4,35 @@ const path = require('path');
 const directoryPath = path.join(__dirname, '../../../docs/_includes/workflow-extensions');
 const outputFile = path.join(__dirname, '../../../docs/_includes/workflow-extensions.md');
 
-fs.readdir(directoryPath, function (err, files) {
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
+try {
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            throw new Error('Unable to scan directory: ' + err);
+        }
 
-    let fileContent = '';
+        let fileContent = '';
 
-    files.forEach(function (file) {
-        // Ignore if not a markdown file
-        if(path.extname(file) !== '.md') return;
+        files.forEach(function (file) {
+            // Ignore if not a markdown file
+            if(path.extname(file) !== '.md') return;
 
-        // Removing file extension for include command
-        const fileNameWithoutExtension = path.parse(file).name;
+            // Removing file extension for include command
+            const fileNameWithoutExtension = path.parse(file).name;
 
-        fileContent += `{{ include "${fileNameWithoutExtension}" }}\n\n---\n\n`;
+            fileContent += `{{ include "${fileNameWithoutExtension}" }}\n\n---\n\n`;
+        });
+
+        // Remove the last extra line and '---'
+        fileContent = fileContent.slice(0, -5);
+
+        // Write to the output file
+        try {
+            fs.writeFileSync(outputFile, fileContent);
+            console.log(`Successfully written to ${outputFile}`);
+        } catch (err) {
+            throw new Error('Unable to write to file: ' + err);
+        }
     });
-
-    // Remove the last extra line and '---'
-    fileContent = fileContent.slice(0, -5);
-
-    // Write to the output file
-    fs.writeFileSync(outputFile, fileContent);
-});
+} catch (err) {
+    console.error(err);
+}
