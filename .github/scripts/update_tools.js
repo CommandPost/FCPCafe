320bd03c615593e07b5cda2fde31ff7f6fc8a803
Chris Hocking <latenitefilms@users.noreply.github.com>
2023-06-07 12:46:05 +1000

    Updated Tools Script

--- .github/scripts/update_tools.js
+++ .github/scripts/update_tools.js
@@ -5,40 +5,52 @@ const directoryPath = path.join(process.env.GITHUB_WORKSPACE, 'docs/_includes/to
 const outputFile = path.join(process.env.GITHUB_WORKSPACE, 'docs/_includes/tools.md');
 
 try {
-  fs.readdir(directoryPath, function (err, files) {
-    if (err) {
-      throw new Error('Unable to scan directory: ' + err);
-    }
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            throw new Error('Unable to scan directory: ' + err);
+        }
 
-    let fileContent = '';
+        let fileContent = '';
+        let lastInitial = '';
 
-    // Sort filenames in alphabetical order
-    files.sort();
+        // Sort files array to guarantee alphabetical order
+        files.sort();
 
-    files.forEach(function (file) {
-      // Ignore if not a markdown file
-      if(path.extname(file) !== '.md') return;
+        files.forEach(function (file) {
+            // Ignore if not a markdown file
+            if(path.extname(file) !== '.md') return;
 
-      // Removing file extension for include command
-      const fileNameWithoutExtension = path.parse(file).name;
+            // Removing file extension for include command
+            const fileNameWithoutExtension = path.parse(file).name;
 
-      // Construct the title from the filename
-      const title = fileNameWithoutExtension.split('').map((c, i) => i === 0 ? c.toUpperCase() : c).join('');
+            // Extract first character of the file name
+            const currentInitial = fileNameWithoutExtension.charAt(0).toUpperCase();
 
-      fileContent += `## ${title}\n\n{{ include "${fileNameWithoutExtension}" }}\n\n---\n\n`;
+            // If current initial is different from last initial, then add a new section
+            if (currentInitial !== lastInitial) {
+                // Don't add '---' for the first section
+                if (lastInitial !== '') {
+                    fileContent += '\n---\n\n';
+                }
+
+                fileContent += `## ${currentInitial}\n\n`;
+                lastInitial = currentInitial;
+            }
+
+            fileContent += `{{ include "${fileNameWithoutExtension}" }}\n\n---\n\n`;
+        });
+
+        // Remove the last extra line and '---'
+        fileContent = fileContent.slice(0, -5);
+
+        // Write to the output file
+        try {
+            fs.writeFileSync(outputFile, fileContent);
+            console.log(`Successfully written to ${outputFile}`);
+        } catch (err) {
+            throw new Error('Unable to write to file: ' + err);
+        }
     });
-
-    // Remove the last extra line and '---'
-    fileContent = fileContent.slice(0, -5);
-
-    // Write to the output file
-    try {
-      fs.writeFileSync(outputFile, fileContent);
-      console.log(`Successfully written to ${outputFile}`);
-    } catch (err) {
-      throw new Error('Unable to write to file: ' + err);
-    }
-  });
 } catch (err) {
-  console.error(err);
+    console.error(err);
 }
