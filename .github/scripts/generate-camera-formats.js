765b55de5029201e8b2bfa54f3874f16569b831b
Chris Hocking <latenitefilms@users.noreply.github.com>
2023-06-07 17:31:50 +1000

    Added Camera Formats

---
+++ .github/scripts/generate-camera-formats.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = path.join(process.env.GITHUB_WORKSPACE, 'docs/_includes/camera-formats');
+const outputFile = path.join(process.env.GITHUB_WORKSPACE, 'docs/_includes/generated-camera-formats.md');
+
+try {
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            throw new Error('Unable to scan directory: ' + err);
+        }
+
+        let fileContent = '';
+        let lastInitial = '';
+
+        // Sort files array to guarantee alphabetical order
+        files.sort();
+
+        files.forEach(function (file) {
+            // Ignore if not a markdown file
+            if(path.extname(file) !== '.md') return;
+
+            // Removing file extension for include command
+            const fileNameWithoutExtension = path.parse(file).name;
+
+            // Extract first character of the file name
+            const currentInitial = fileNameWithoutExtension.charAt(0).toUpperCase();
+
+            // If current initial is different from last initial, then add a new section
+            if (currentInitial !== lastInitial) {
+                // Don't add '---' for the first section
+                if (lastInitial !== '') {
+                    fileContent += '\n---\n\n';
+                }
+
+                fileContent += `## ${currentInitial}\n\n`;
+                lastInitial = currentInitial;
+            }
+
+            fileContent += `{{ include "motion-templates/${fileNameWithoutExtension}" }}\n\n---\n\n`;
+        });
+
+        // Remove the last extra line and '---'
+        fileContent = fileContent.slice(0, -5);
+
+        // Write to the output file
+        try {
+            fs.writeFileSync(outputFile, fileContent);
+            console.log(`Successfully written to ${outputFile}`);
+        } catch (err) {
+            throw new Error('Unable to write to file: ' + err);
+        }
+    });
+} catch (err) {
+    console.error(err);
+}
---
+++ docs/camera-formats.md
@@ -0,0 +1,15 @@
+# Camera Formats
+
+This page contains a collection of the latest Final Cut Pro Camera Formats.
+
+You can find the full list of Cameras supported by Final Cut Pro [on Apple's site](https://support.apple.com/en-us/HT204203){target="_blank"}.
+
+---
+
+!!!
+Is there a camera format missing? [Make an edit!](/contribute/){target="_blank"}
+!!!
+
+---
+
+{{ include "generated-camera-formats" }}
